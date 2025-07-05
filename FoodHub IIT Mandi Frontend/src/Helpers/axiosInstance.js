d8de8b91adf958e3c18c0960ca55b520de284e24
kartavya <kartavyasandhu1@gmail.com>
2025-07-05 22:57:52 +0530

    hardcode backend URL for github pages

--- FoodHub IIT Mandi Frontend/src/Helpers/axiosInstance.js
+++ FoodHub IIT Mandi Frontend/src/Helpers/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create(); // Create a new instance of axios
 
-axiosInstance.defaults.baseURL = import.meta.env.VITE_BACKEND_URL; // Set the base URL
+axiosInstance.defaults.baseURL = import.meta.env.VITE_BACKEND_URL || 'https://web-production-26189.up.railway.app'; // Set the base URL
 axiosInstance.defaults.withCredentials = true; // Allow cookies to be sent with requests
 
 // Performance optimizations
